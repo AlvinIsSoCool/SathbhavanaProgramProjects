7847a49af29bec28926943765fa5d32b579d7e2c
Alvin Alexander <lindageorge5335@gmail.com>
2024-02-20 13:43:16 +0530

    Adding All Files

---
+++ WindowCreation/Main.c
@@ -0,0 +1,5 @@
+#include "Main.h"
+
+INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdline, _In_ INT nShowCmd) {
+	//MessageBox(NULL, "Hello!", "Program3", MB_OK);
+}
